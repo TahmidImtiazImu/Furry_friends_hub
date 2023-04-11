c97b3a11d6d37f38705e3cf25a833d67b589cc89
AhsanulJobayer <ajahsanul19@gmail.com>
2023-04-11 15:37:07 +0600

    signup route added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Product from './pages/Product'
+import Signup from './pages/Signup'
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Homepage/>}  />
             <Route path="/product" element={<Product/>}  />
+            <Route path="/Signup" element={<Signup/>} />
         </Routes>
     </Router>
   )
