3100a9c2cc8b815d72a431ee74f309a472397076
AhsanulJobayer <ajahsanul19@gmail.com>
2023-04-11 21:06:31 +0600

    signup page progress

--- client/src/pages/SIgnup.js
+++ client/src/pages/SIgnup.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Card from './Card'
+
+const joinClick = () => {
+    alert("Successfully joined!");
+}
 
 export const signup = () => {
   return (
-    <div>signup</div>
+    <div>
+        <div className='header_title'>
+            <h1>signup</h1>
+        </div>
+        <div className='wrapper'>
+                <Card 
+                img = "./images/customer.jpg"
+                title = "Cutomer"/>
+                <Card 
+                img = "./images/service_provider.png"
+                title = "Service provider"/>
+        </div>
+        <div className='confirm_button'>
+            <button onClick={joinClick}>Join As Customer</button>
+        </div>
+    </div>
+    
   )
 }
 export default signup
