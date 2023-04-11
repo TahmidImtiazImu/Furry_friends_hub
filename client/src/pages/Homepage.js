3c792171d747137d25740c43c7546962ecc2e942
TahmidImtiazImu <tahmidimtiaz11807048@gmail.com>
2023-04-11 23:22:15 +0600

    homepage progrerssing

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -4,9 +4,10 @@ import {BsSearch,BsPeople} from 'react-icons/bs'
 import styled from "styled-components";
 import {FaCat, FaDog, FaKiwiBird,FaWalking} from "react-icons/fa"
 import {GiRabbit} from "react-icons/gi"
-import {MdPeopleAlt} from "react-icons/md"
+import {MdPeopleAlt,MdOutlineSecurity,MdSentimentSatisfiedAlt} from "react-icons/md"
 import {BsFillCartFill,BsFillHouseFill,BsFacebook,BsWhatsapp,BsInstagram,BsTwitter,BsYoutube} from "react-icons/bs"
 import {AiOutlineSearch} from "react-icons/ai"
+import {GrDeliver} from "react-icons/gr"
 
 const theme = {
   blue: {
@@ -96,10 +97,35 @@ const Homepage = () => {
 
       {/* BODDDDDDDDDDDDDDDDYYYYYYYYYYYYYYYY */}
       <body className="body">
-        <div>
-          Body
-        </div>
+        <ul className="images">
+          <li><img src={"/images/Cat.jpeg"} alt="React Image" className="pet_image"/></li>
+          <li><img src={"/images/dog.jpg"} alt="React Image" className="pet_image"/></li>
+          <li><img src={"/images/rabbit.webp"} alt="React Image" className="pet_image"/></li>
+        </ul>
+        <ul className="images2">
+        <li><img src={"/images/bird.jpg"} alt="React Image" className="pet_image2"/></li>
+        <li><img src={"/images/pet_sitter.jpg"} alt="React Image" className="pet_image2"/></li>
+        </ul>
+
+        <ul className="body_writing">
+        <ul className="delivery">
+        <li className="list"> <a href><GrDeliver className="servicelist"/> </a></li>
+        <li> <p> Delivry all over bangladesh. Fast delivery is ensured. </p></li> 
+        </ul>
+        <ul className="security">
+        <li className="list"> <a href><MdOutlineSecurity className="servicelist"/> </a></li>
+        <li> <p> Provide best security for you 
+          and your product. </p></li> 
+        </ul>
+        <ul className="satisfied">
+        <li className="list"> <a href><MdSentimentSatisfiedAlt className="servicelist"/> </a></li>
+        <li> <p> Reliable and satisfiable services. Give ensurity of best product and will meet your need. </p></li> 
+        </ul>
+        </ul>
+        
       </body>
+
+
 
       {/* FOOOOOOOOOOOOOOOOTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEERRRRRRRRRRRRR */}
       <footer className="footer">
