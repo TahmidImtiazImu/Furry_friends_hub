1864ca9c220ec5c14322ef1faf3d92116d3e21d7
AhsanulJobayer <ajahsanul19@gmail.com>
2023-04-12 02:21:29 +0600

    Customer sign in

--- client/src/pages/CustomerSignup.js
+++ client/src/pages/CustomerSignup.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import './CustomerSignup.css'
 
 export const CustomerSignup = () => {
+    const [agreement, setAgreement] = useState(false);
+
+    const handleChange = (event) => {
+        setAgreement(event.target.checked);
+    }
+
+    const signUp = () => {
+        alert("SUccessfully Signed Up!")
+    }
+
   return (
-    <div>CustomerSignup</div>
+    <div className="customersignWhole">
+        <h1>Sign Up as a Customer</h1>
+        <div className="CustomerSignBorder">
+            <div className='CustomerSign'>
+                <div className='customernameinput'>
+                    *Name: <input type="text" placeholder="Name" className="inputName"/>
+                </div>
+                <div className = 'customermailinput'> 
+                    *E-mail: <input type="text" placeholder="Email" className="inputmail"/>
+                </div>
+                <div className = 'customerpassinput'> 
+                    *Password: <input type="text" placeholder="Password" className="inputpass"/>
+                </div>
+                <div className = 'customerrepassinput'> 
+                    *Confirm Passwoed: <input type="text" placeholder="Password" className="inputpass2"/>
+                </div>
+                <p className='customeragree'>
+                    <input type='checkbox' name="agreement" onChange={handleChange}/> I agree to the <a href='#'>Terms of use</a> and <a href='#'>Privacy Policy</a>.
+                </p>
+                <div className="customersignwhole">
+                    <button className='customersigned' onClick={signUp} disabled={!agreement}>SIgn Up</button> <a href='#'>Learn more</a>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
