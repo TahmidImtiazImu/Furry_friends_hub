13b184d18d29f677dedf94e0b04b55d42e5255f2
AhsanulJobayer <ajahsanul19@gmail.com>
2023-04-13 10:46:25 +0600

    product popup inital stage

--- client/src/pages/OneProduct.js
+++ client/src/pages/OneProduct.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
+import {Routes, Route, useNavigate, Navigate} from 'react-router-dom';
 import "./OneProduct.css"
+import ProductPopupInfo from "./productPopupInfo"
 
 const OneProduct = () => {
+  const navigate = useNavigate();
+  const[popuproduct,setpoproduct] = useState(false);
+
+  const click = () => {
+    // alert("clicked!!");
+    setpoproduct(true);
+  }
+
   return ( 
 
-    <div className="oneproduct">
+    <div className="oneproduct" onClick={click}>
+      {popuproduct && <ProductPopupInfo/>}
         <div className="stock">
          Stock:3
         </div>
---
+++ client/src/pages/productPopupInfo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './productPopupInfo.css'
+
+export const productPopupInfo = () => {
+  return (
+    <div className='ProductInfo'>productPopupInfo</div>
+  )
+}
+
+export default productPopupInfo
