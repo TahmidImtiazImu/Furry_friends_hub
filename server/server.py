9fda128ed310fb0070f03c6aca63999cf24d247f
TahmidImtiazImu <tahmidimtiaz11807048@gmail.com>
2023-04-29 23:45:06 +0600

    cart_page_run

--- server/server.py
+++ server/server.py
@@ -28,6 +28,11 @@ def customerSignup():
 def ServiceSignup():
     return app.send_static_file('index.html')
 
+
+@app.route('/Cart')
+def cart():
+    return app.send_static_file('index.html')
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="5000")
 
